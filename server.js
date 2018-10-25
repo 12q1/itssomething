625be6d4c36317bf325f4b50ec45fd7e86c3fb37
12q1 <thomasham89@gmail.com>
2018-10-25 15:46:20 +0200

    mistake with socketArray fixed

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ io.on('connection', function (socket) {
   socket.on('new player', function () {
     players[socket.id] = {
       socket: socket.id,
-      name: "player"+number++,
+      name: "player" + number++,
       x: getRandomNumber(800),
       y: getRandomNumber(400), //these coordinates are the starting position of a player
       color: playerColors[getRandomNumber(playerColors.length)],
@@ -67,10 +67,10 @@ io.on('connection', function (socket) {
       socketArray = Object.keys(players) //create an array of socket IDs
       dataArray = Object.values(players) //create an array of data objects (eg. xcoords/ycoords)
       for (i = 0; i < dataArray.length; i++) { //for each index of dataArray 
-        if (player.x+playerSize > dataArray[i].x && player.x < dataArray[i].x + playerSize && socket.id !== socketArray[i]) { //if player x is the same as one of the data packets and the socketIDs don't match
-          if (player.y+playerSize > dataArray[i].y && player.y < dataArray[i].y + playerSize && socket.id !== socketArray[i]) { //if player y is the same as one of the data packets and the socketIDs don't match
+        if (player.x + playerSize > dataArray[i].x && player.x < dataArray[i].x + playerSize && socket.id !== socketArray[i]) { //if player x is the same as one of the data packets and the socketIDs don't match
+          if (player.y + playerSize > dataArray[i].y && player.y < dataArray[i].y + playerSize && socket.id !== socketArray[i]) { //if player y is the same as one of the data packets and the socketIDs don't match
             console.log(`a collision has occurred on both axis between ${player.socket} and ${socketArray[i]}`)
-            if(player.infected===false&&socketArray[i].infected===true){
+            if (player.infected === false && dataArray[i].infected === true) {
               player.infected = true
               player.color = 'red'
             }
